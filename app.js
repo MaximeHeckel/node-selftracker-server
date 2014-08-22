1de20c5ca47954b34e489991d8fa4d70f2a3726b
MaximeHeckel <heckelmaxime@gmail.com>
2014-08-22 19:24:23 +0200

    initiate model file

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var express = require('express')
   , app = express()
   , jf = require('jsonfile')
+  , mongoose = require('mongoose')
+  , database = require('./config/database')
   , Fitbit = require('fitbit');
+
+//Configuration
+
+mongoose.connect(database.url)
 
 app.use(express.cookieParser());
 app.use(express.session({secret: 'hekdhthigib'}));
