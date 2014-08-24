3ef316160af8abc63a48165626d53ed2003d92af
MaximeHeckel <heckelmaxime@gmail.com>
2014-08-24 12:37:46 +0200

    cleaned fitbit api code

--- app/api/fitbit.js
+++ app/api/fitbit.js
@@ -4,16 +4,16 @@ var dateformat = require('dateformat');
 
 module.exports = function(jf){
 
-  var now = new Date();
+var now = new Date();
 
-  function readToken(file, callback){
-        jf.readFile(file, function(err,obj){
-          if(err) callback(err);
-          callback(null,obj);
-        })
-  }
+function readToken(file, callback){
+      jf.readFile(file, function(err,obj){
+        if(err) callback(err);
+        callback(null,obj);
+      })
+}
 
-  readToken('./token.json',function(err,res){
+readToken('./token.json',function(err,res){
   var client = new Fitbit(
       '44fde411b9fc4a79a20ad3f50c0961dd'
     , 'a306186235724a2fb11d3c5fa82d6eed'
@@ -24,26 +24,26 @@ module.exports = function(jf){
       }
 
   );
-    client.getActivities(function (err, activities) {
-      //ACTIVITY UPDATE AND CREATE
-      Activity.findOne({},{}, {sort:{'date': -1}}, function(err,lastActivity){
-        if(err) console.log(err)
-        //console.log(lastActivity)
-        //console.log(dateformat(lastActivity.date,"m/dd/yy"))
-        if(dateformat(lastActivity.date,"m/dd/yy")==dateformat(now,"m/dd/yy")){
-          lastActivity.update({
-            steps: activities._attributes.summary.steps,
-            activitymin: activities._attributes.summary.fairlyActiveMinutes,
-            calories: activities._attributes.summary.caloriesOut
-          },function(err,Activity){
-            if(err) console.log(err)
-          });
-        } else {
-          Activity.create({
-            steps: activities._attributes.summary.steps,
-            activitymin: activities._attributes.summary.fairlyActiveMinutes,
-            calories: activities._attributes.summary.caloriesOut
-          },function(err,Activity){
+  client.getActivities(function (err, activities) {
+    //ACTIVITY UPDATE AND CREATE
+    Activity.findOne({},{}, {sort:{'date': -1}}, function(err,lastActivity){
+      if(err) console.log(err)
+      //console.log(lastActivity)
+      //console.log(dateformat(lastActivity.date,"m/dd/yy"))
+      if(dateformat(lastActivity.date,"m/dd/yy")==dateformat(now,"m/dd/yy")){
+        lastActivity.update({
+          steps: activities._attributes.summary.steps,
+          activitymin: activities._attributes.summary.fairlyActiveMinutes,
+          calories: activities._attributes.summary.caloriesOut
+        },function(err,Activity){
+          if(err) console.log(err)
+        });
+      } else {
+        Activity.create({
+          steps: activities._attributes.summary.steps,
+          activitymin: activities._attributes.summary.fairlyActiveMinutes,
+          calories: activities._attributes.summary.caloriesOut
+        },function(err,Activity){
             if(err) console.log(err);
           });
         }
