9d9c596979672baf39b0422496c282394958bc4d
MaximeHeckel <heckelmaxime@gmail.com>
2014-09-03 18:16:51 +0200

    Update Activity model

--- app/models/activity.js
+++ app/models/activity.js
@@ -4,5 +4,7 @@ module.exports = mongoose.model('Activity', {
 	date: {type: Date, default: Date.now},
    steps: Number,
    activitymin: Number,
-   calories: Number
+   calories: Number,
+	distance: Number,
+	rundistance: Number
 });
