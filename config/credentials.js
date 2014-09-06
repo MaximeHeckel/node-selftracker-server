1288fde1f3ac998b49cfa1639cb83ec4b813a0cf
MaximeHeckel <heckelmaxime@gmail.com>
2014-09-06 16:09:40 +0200

    cred files for deployment

--- config/credentials.js
+++ config/credentials.js
@@ -1,10 +1,10 @@
 module.exports = {
 
-  account: 'admin',
-  password: 'admin',
-  host : '127.0.0.1', //default is localhost set your host here
-  fitbitClientID : '',
-  fitbitClientSecret : '',
-  runkeeperClientID : '',
-  runkeeperClientSecret : ''
+  account : 'maximeheckel',
+  password : 'Max15051992',
+  host : 'https://maxselftrackingserver.appsdeck.eu',
+  fitbitClientID : '169f5cf6fe9c45a0ac96b92dceaf103f',
+  fitbitClientSecret : 'a2177195b3ca45b1885c2129f21142eb',
+  runkeeperClientID : '4f4d7c07bfc6405fad65f6c922adc3ec',
+  runkeeperClientSecret : '96d66d8a34484cae96ce6efd70adfe21'
 }
