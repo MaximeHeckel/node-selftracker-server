819ad0b964edcd35f753f6b6590bc06cdae8ab6d
MaximeHeckel <heckelmaxime@gmail.com>
2014-09-06 16:17:38 +0200

    fix missing :

--- app/api/runkeeper.js
+++ app/api/runkeeper.js
@@ -66,7 +66,7 @@ exports.storeLastRun = function(port){
           if(dateformat(lastRun.date,"m/dd/yy")!=dateformat(now,"m/dd/yy")){
             console.log("Creating new Run")
         request.get({
-          uri: credentials.host+port+lastActivity.urilastactivity, //Need to change that ( doesn't work if we call directly the Runkeeper API ??)
+          uri: credentials.host+":"+port+lastActivity.urilastactivity, //Need to change that ( doesn't work if we call directly the Runkeeper API ??)
           headers: {
             'Accept': 'application/vnd.com.runkeeper.FitnessActivityFeed+json',
             'Authorization': 'Bearer ' + res.Token
