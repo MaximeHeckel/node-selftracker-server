48137422e45fb934b86f86812662f2f51bfd27fe
MaximeHeckel <heckelmaxime@gmail.com>
2014-09-07 09:54:02 +0200

    change host

--- config/credentials.js
+++ config/credentials.js
@@ -2,7 +2,7 @@ module.exports = {
 
   account : 'maximeheckel',
   password : 'Max15051992',
-  host : 'http://127.0.0.1:3000',
+  host : 'https://maxselftrackingserver.appsdeck.eu',
   fitbitClientID : '169f5cf6fe9c45a0ac96b92dceaf103f',
   fitbitClientSecret : 'a2177195b3ca45b1885c2129f21142eb',
   runkeeperClientID : '4f4d7c07bfc6405fad65f6c922adc3ec',
